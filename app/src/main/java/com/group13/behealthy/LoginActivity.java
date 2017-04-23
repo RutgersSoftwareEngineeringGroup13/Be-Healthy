c971b1fff3138c98d95dff6c52ed18de5e269f80
Bonypatel <bonyp96@gmail.com>
2017-04-23 15:16:53 -0400

    Update LoginActivity.java

--- app/src/main/java/com/group13/behealthy/LoginActivity.java
+++ app/src/main/java/com/group13/behealthy/LoginActivity.java
@@ -12,10 +12,20 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.firebase.client.FirebaseApp;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private ProgressBar progressBar;
     private Button btnSignup, btnLogin, btnReset;
+    FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,13 +110,38 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-                                    Intent intent = new Intent(LoginActivity.this, Select_Plan.class);
-                                    startActivity(intent);
-                                    finish();
+
+                                    DatabaseReference mDatabase;
+// ...
+                                    mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                                    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    String uid1 = user.getUid();
+
+                                    String key = mDatabase.child("UserPlan").push().getKey();
+
+
+
+                                    if(key != null)
+                                    {
+                                        Intent intent = new Intent(LoginActivity.this, Progress_Dashboard.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Intent intent1 = new Intent(LoginActivity.this, Select_Plan.class);
+                                        startActivity(intent1);
+                                        finish();
+                                    }
+
+
                                 }
                             }
                         });
             }
         });
     }
+
+
 }
+
